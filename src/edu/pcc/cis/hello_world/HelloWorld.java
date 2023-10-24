4408e17ddb8a734edd965105178f87acf837eb12
Heidi Young <heidi.young@pcc.edu>
2023-10-23 18:30:04 -0700

    Print date added

--- src/edu/pcc/cis/hello_world/HelloWorld.java
+++ src/edu/pcc/cis/hello_world/HelloWorld.java
@@ -1,5 +1,7 @@
 package edu.pcc.cis.hello_world;
 
+import java.util.Date;
+
 /**
  * Sample project for 233J
  *
@@ -9,6 +11,7 @@ package edu.pcc.cis.hello_world;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        System.out.println(new Date());
     }
 
 }

bd994e262979c12ea2baabd1560eea534fd05b4b
Heidi Young <heidi.young@pcc.edu>
2023-10-23 18:27:25 -0700

    Added main method to print Hello

--- src/edu/pcc/cis/hello_world/HelloWorld.java
+++ src/edu/pcc/cis/hello_world/HelloWorld.java
@@ -2,9 +2,13 @@ package edu.pcc.cis.hello_world;
 
 /**
  * Sample project for 233J
- * 
+ *
  * @author Heidi Young
  * @version 2023.10.23
  */
 public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+
 }
